2931dd8155fbb40449e7a87f2c31b9e0e8e63d44
Ch3ru09 <xliang0905@gmail.com>
2023-04-16 23:37:38 -0400

    select commands animation

--- src/components/Navbar/Command/index.tsx
+++ src/components/Navbar/Command/index.tsx
@@ -1,6 +1,6 @@
 export default function Command({ name }: Props) {
   return (
-    <div class="bg-transparent h-10 w-[90%] flex flex-col mb-3 rounded-md text-center justify-center hover:commands">
+    <div class="commands hover:commands">
       <p>{name}</p>
     </div>
   );
